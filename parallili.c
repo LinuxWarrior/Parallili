4fe58ec80ed0c0118fc396549dbb9d8d56bd51dd
FaNiX <indoril69@gmail.com>
2012-06-10 21:03:21 +0300

    added comments

--- parallili.c
+++ parallili.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <math.h>
 
-#define HOURS 24
+#define HOURS 24  //problepseis
 #define ROWS 8
 #define COLUMNS 8
 #define MAX_X 7
@@ -11,9 +11,11 @@
 #define MIN_X 7
 #define MIN_Y 7
 
+/* dhmiourgia structs isa me tis problepseis mas (HOURS) */
+/* pinakas data me ta stoixeia ths katastasis, thermokrasias, entasis */
 struct K
 {
-	unsigned short i;
+	unsigned short i; 			//pithanws axrhsto
 	int data[ROWS][COLUMNS];
 } katastasi[HOURS];
 
@@ -29,15 +31,16 @@ struct T
 	int data[ROWS][COLUMNS];
 } therm[HOURS];
 
+/* ----------------------------------------------------------------- */
+
 void init() {
 	int i, j, n;
 	FILE *file; 
 	
-	
-	for (i = 0; i < HOURS; i++) {
-		katastasi[i].i = i;
-		therm[i].i = i;
-		entasi[i].i = i;
+	for (i = 0; i < HOURS; i++) {	//arxikopoihsh
+		katastasi[i].i = i;			//tou element i pou deixnei se poia katastasi eimaste
+		therm[i].i = i;				// gia debugging
+		entasi[i].i = i;			// NA TO SBHSW
 	}
 	
 	for (i = 0; i < HOURS; i++) {
@@ -45,14 +48,14 @@ void init() {
 		
 		for (j = 0; j < ROWS; j++) {
 			for (n = 0; n < COLUMNS; n++) {
-				katastasi[i].data[j][n] = rand() % 5;
-				therm[i].data[j][n] = 22;
-				entasi[i].data[j][n] = 0;
+				katastasi[i].data[j][n] = rand() % 5; //arxikopoihsh pinakwn katastasis me tyxaia timh apo 0 ws 5
+				therm[i].data[j][n] = 22;			//arxikopoihsh thermokrasias me 22
+				entasi[i].data[j][n] = 0;			//........... entasi me 0
 				
-				if (i == 0) {
+				if (i == 0) { 
 					file = fopen("map.000","a+"); 
-					fprintf(file,"%d", katastasi[i].data[j][n]);
-					fclose(file);
+					fprintf(file,"%d", katastasi[i].data[j][n]); //apothikeusi arxikou pinaka katastasis
+					fclose(file);								// se arxeio map.000
 				} 
 			}
 		}	
@@ -82,13 +85,15 @@ int main(int argc, char *argv[]) {
 	char buf[24] = { 0 };
 	
 	srand(time(NULL));
-	init();
+	init(); //arxikopoihsh
 	
+	/* loops gia gemisma epomenwn katastasewn */
 	for (i = 1; i < HOURS; i++) {
 		for (j = 0; j < ROWS; j++) {
 			for (n = 0; n < COLUMNS; n++) {
 				katastasi[i].data[j][n] =  abs((int) (katastasi[i-1].data[j][n] + sin(7*i) * 3) % 6);
 				
+				/* euresi geitonikwn stoixeiwn */
 				startPosX = (j-1 < MIN_X) ? j : j-1;
 				startPosY = (n-1 < MIN_Y) ? n: n-1;
 				endPosX = (j+1 > MAX_X) ? j : j+1;
@@ -96,13 +101,17 @@ int main(int argc, char *argv[]) {
 				
 				for(rowNum = startPosX; rowNum <= endPosX; rowNum++){
 					for(colNum = startPosY; colNum <= endPosY; colNum++){
+						/* elegxos gia apokliseis megalyteres tou 1 */
 						if (abs((katastasi[i].data[j][n] - katastasi[i-1].data[rowNum][colNum])) > 1) {
-							opt = 1;
-							//sum += katastasi[i-1].data[rowNum][colNum];
-							//count++;
+							opt = 1; //flag gia apoklish
 						}
+						/* ------------------------------------------ */
 					}
 				}
+				/* -------------------------------------------------- */
+				/* an exoume megalyterh apoklish apo 1 */
+				/* briskoume to meso oro twn geitonikwn stoixeiwn */
+				/* kai antikathistoume thn timh */ 
 				if (opt == 1) {
 					for(rowNum = startPosX; rowNum <= endPosX; rowNum++){
 						for(colNum = startPosY; colNum <= endPosY; colNum++){
@@ -114,6 +123,7 @@ int main(int argc, char *argv[]) {
 					sum = 0;
 					count = 0;	
 				}
+				/* _________________________________________________ */
 				opt = 0;
 				therm[i].data[j][n] =  (22 + (sin(6.5 * i ) * 10) - (katastasi[i-1].data[j][n] / 4)) + (therm[i-1].data[j][n]/50);
 				entasi[i].data[j][n] =  (entasi[i-1].data[j][n]/4) + (sin(7 * i) * 3);
